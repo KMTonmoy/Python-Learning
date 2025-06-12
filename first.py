cd7dc6aefbcc6cdafd1376c2bafb3122f0554c93
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-06-12 11:37:22 +0600

    Second PYthon Added

--- first.py
+++ first.py
@@ -1,1 +1,5 @@
 print("Hello World")
+print(5)
+print('Bay')
+print(17*13)
+print("Tonmoy A.")
---
+++ second.py
@@ -0,0 +1,4 @@
+# This is MY Comment
+# Author: Tonmoy A.
+
+print("Hay I am a Good Boy\nViwer Is also A good Boy/Girl")
