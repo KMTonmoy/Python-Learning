7406109f9626ecb79db287eca26d9af8cf9f3fd5
KMTonmoy <tonmoyahamed2009@gmail.com>
2025-06-13 22:35:13 +0600

    python Bool data added

---
+++ DataType.py
@@ -0,0 +1,31 @@
+# int type data
+hablu= 450
+print(type(hablu))
+
+# floating type data 
+
+gablu = 4.2
+print(type(gablu))
+
+
+# complex data type
+kodu =420j
+print(type(kodu))
+
+
+# str type data
+YourName = 'Hablu'
+MyName = 'Tonmoy'
+print(YourName + MyName)
+
+# Bool Type data
+
+HabluBool = True
+HabluBool1 = False
+HabluBool2 = None
+print(type(HabluBool))
+
+x=8
+y=10
+print(x == y) 
+ 
